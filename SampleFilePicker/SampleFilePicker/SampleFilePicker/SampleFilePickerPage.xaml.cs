99dc490edf40cf14a0a39d09c20cdf7e4c23f7ab
畑中 拓 <BOG-Taku_Hatanaka@jmas.co.jp>
2017-02-11 20:56:25 +0900

    サンプルの作成

---
+++ SampleFilePicker/SampleFilePicker/SampleFilePicker/SampleFilePickerPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Plugin.FilePicker;
+using Plugin.FilePicker.Abstractions;
+using Xamarin.Forms;
+
+namespace SampleFilePicker
+{
+    public partial class SampleFilePickerPage : ContentPage
+    {
+        public SampleFilePickerPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            this.btnFileSelect.Clicked += async (sender, e) =>
+            {
+                // ファイルピッカーを開く
+                IFilePicker filePicker = CrossFilePicker.Current;
+                FileData fileData = await filePicker.PickFile();
+
+                if (fileData != null)
+                {
+                    // 選択したファイルの名称を表示
+                    this.lblFileName.Text = fileData.FileName;
+                }
+            };
+        }
+    }
+}
